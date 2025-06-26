7e80fd99d5663bd3a2c99023784a886888ce2166
mk293822 <mkt293822@gmail.com>
2025-06-26 05:09:06 +0000

    begin

---
+++ resources/js/components/input-error.tsx
@@ -0,0 +1,38 @@
+import { cn } from '@/lib/utils';
+import { useEffect, useState, type HTMLAttributes } from 'react';
+
+export default function InputError({ message, className = '', ...props }: HTMLAttributes<HTMLParagraphElement> & { message?: string }) {
+
+    const match = message?.match(/\d+(?= seconds)/);
+
+    const initialSeconds = match ? parseInt(match[0]) : null;
+    const [displayMessage, setDisplayMessage] = useState<string | undefined>(message);
+
+    useEffect(() => {
+        if (!initialSeconds) return;
+
+        setDisplayMessage(`Too many login attempts. Please try again in ${initialSeconds} seconds.`);
+
+        let seconds = initialSeconds;
+
+        const interval = setInterval(() => {
+                if (seconds && seconds > 1) {
+                    seconds -= 1;
+                    setDisplayMessage(`Too many login attempts. Please try again in ${seconds} seconds.`);
+                } else {
+                    clearInterval(interval);
+                    setDisplayMessage('You can try again now.');
+                }
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [message, initialSeconds]);
+
+
+
+    return message ? (
+        <p {...props} className={cn('text-sm text-red-600 dark:text-red-400', className)}>
+            {displayMessage}
+        </p>
+    ) : null;
+}
